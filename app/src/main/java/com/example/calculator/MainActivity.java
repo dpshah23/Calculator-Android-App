72b7d921c57c0b58acec6a504272ab1c3ceae9d6
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2024-08-21 18:40:00 +0530

    Added Operand code

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -17,7 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnno1,btnno2,btnno3,btnno4,btnno5,btnno6,btnno7,btnno8,btnno9,btnno0;
 
-    Button multiply,addition,divide,percentage,dot,del,clear,subtraction;
+    Button multiply,addition,divide,percentage,dot,del,clear,subtraction,equal;
+
+    String dispequation = "";
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -45,6 +48,66 @@ public class MainActivity extends AppCompatActivity {
         addition=findViewById(R.id.btnAdd);
         multiply=findViewById(R.id.btnMultiply);
         divide=findViewById(R.id.btnDivide);
-        subtraction=findViewById(R.id.btnSubtract)
+        subtraction=findViewById(R.id.btnSubtract);
+
+        equal=findViewById(R.id.btnEquals);
+
+        del=findViewById(R.id.btnDelete);
+
+        percentage=findViewById(R.id.btnPercentage);
+        clear=findViewById(R.id.btnClear);
+
+        dot=findViewById(R.id.btnDot);
+
+        display=findViewById(R.id.tvDisplay);
+
+        String dispequation = "";
+
+
+        btnno0.setOnClickListener(v -> appendDigit("0"));
+        btnno1.setOnClickListener(v -> appendDigit("1"));
+        btnno2.setOnClickListener(v -> appendDigit("2"));
+        btnno3.setOnClickListener(v -> appendDigit("3"));
+        btnno4.setOnClickListener(v -> appendDigit("4"));
+        btnno5.setOnClickListener(v -> appendDigit("5"));
+        btnno6.setOnClickListener(v -> appendDigit("6"));
+        btnno7.setOnClickListener(v -> appendDigit("7"));
+        btnno8.setOnClickListener(v -> appendDigit("8"));
+        btnno9.setOnClickListener(v -> appendDigit("9"));
+        dot.setOnClickListener(v -> appendDigit("."));
+
+        addition.setOnClickListener(v -> setOperator("+"));
+        multiply.setOnClickListener(v -> setOperator("*"));
+        subtraction.setOnClickListener(v -> setOperator("-"));
+        divide.setOnClickListener(v -> setOperator("/"));
+        percentage.setOnClickListener(v -> setOperator("%"));
+
+        clear.setOnClickListener(v -> {
+            display.setText("");
+        });
+
+        del.setOnClickListener(v -> deleteLastchar());
+
+
     }
+
+    private void deleteLastchar(){
+
+        if (!dispequation.isEmpty()) {
+            dispequation = dispequation.substring(0, dispequation.length() - 1);
+            display.setText(dispequation.isEmpty() ? "0" : dispequation);
+        }
+
+    }
+
+    private void appendDigit(String digit) {
+        dispequation += digit;
+        display.setText(dispequation);
+    }
+
+    private void setOperator(String op) {
+        dispequation+=op;
+        display.setText(dispequation);
+    }
+
 }
